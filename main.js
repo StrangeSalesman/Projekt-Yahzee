862fa3836d63b46ed1e717ca613d51c566294b50
StrangeSalesman <113688686+StrangeSalesman@users.noreply.github.com>
2025-10-29 15:18:37 +0100

    change from 200 to 100 points to win

--- main.js
+++ main.js
@@ -414,7 +414,7 @@ class YahtzeeGame {
             player.bonusPoints()
         } else if (player.remainingSection.upperSection.length === 0 && player.remainingSection.lowerSection.length === 0) {
             // In case all sections are filled, we want to end the game
-            if(player.state.points.total >= 200) {
+            if(player.state.points.total >= 100) {
                 alert(`Fine you beat me! Your final score is ${player.state.points.total}. The Pasword is "Würfelglück"`)
             }
             else {
